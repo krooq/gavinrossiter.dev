cd9ee64c4c83591778ca55913a0857492b3a5cfe
Gavin Rossiter <rossiter.gavin@gmail.com>
2020-07-28 07:56:04 +1000

    animations-dodgy

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 import { Map } from 'immutable';
 import { useGesture, useDrag } from 'react-use-gesture'
-import { useSpring, animated, to, config } from 'react-spring';
+import { useSpring, animated, to, config, Spring } from 'react-spring';
 import { FullGestureState, Vector2 } from 'react-use-gesture/dist/types';
 import useWindowDimensions from './useWindowDimensions';
+import { createGlobalStyle } from 'styled-components';
 
 type vec2 = [number, number]
 
@@ -18,36 +19,45 @@ type Insets = { top: number, right: number, bottom: number, left: number }
 // Transform
 type Transform = { translate: vec2, scale: vec2 }
 
+type Axis = "vertical" | "horizontal"
 
 // Panel
-type Panel = { id: string, insets: Insets }
+type Panel = { id: string, insets: Insets, selected: boolean }
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    --main-background-color: #0f0f0f;
+    --panel-background-color: #1e1e1e;
+    --panel-highlight-color: #4e4e4e
+  }
+`
+const styles = {
+  main_background_color: "#0f0f0f",
+  panel_background_color: "#1e1e1e",
+  panel_highlight_color: "#4f4f4f",
+  toolbar_background_color: "#0f0f0f",
+  toolbar_button_background_color: "#1e1e1e",
+  toolbar_button_active_background_color: "#4f4f4f",
+}
 
 // App
 function App() {
   const { w: windowWidth, h: windowHeight } = useWindowDimensions();
-  const defaultPanel = { id: uuidv4(), insets: { top: 0, right: 0, bottom: 0, left: 0 } }
-  const [panels, setPanels] = useState(Map<string, Panel>().set(defaultPanel.id, defaultPanel))
-  const [target, setEvent] = useTargetElement()
-  const [{ splitTranslate }, setSplitTranslate] = useSpring(() => ({ splitTranslate: [0, 0] }))
-  const [{ splitScale }, setSplitScale] = useSpring(() => ({ splitScale: [1, 1] }))
 
-  const splitGuide = useDrag(e => {
-    let panel = target
-    if (panel != null) {
-      const [x0, y0, w, h] = computedBounds(panel)
-      let [ex, ey] = e.xy
-      var sx = 2
-      var sy = h
-      var tx = clampInterval({ left: ex, width: sx }, { left: x0, width: w })
-      var ty = x0 + h / 2
-      setSplitTranslate({ splitTranslate: [tx, ty], immediate: true })
-      setSplitScale({ splitScale: [sx, sy] })
-    }
-  })
+  const defaultPanel = { id: uuidv4(), insets: { top: 0, right: 0, bottom: 0, left: 0 }, selected: false }
+  const [panels, setPanels] = useState(Map<string, Panel>().set(defaultPanel.id, defaultPanel))
+  // const [target, setGesture] = useTargetElement()
 
   const bind = useGesture({
-    onDragStart: setEvent,
-    // onMoveStart: setEvent,
+    onDragStart: gesture => {
+      let panel = panels.get(gestureTarget(gesture)?.id ?? '')
+      if (panel?.id != null) {
+        setPanels(panels.update(panel.id, p => ({ ...p, selected: !p.selected })))
+      }
+      // setGesture(null)
+    },
+    // onDragStart: setGesture,
+    // onMoveStart: setGesture,
     // onDrag: e => {
     //   var [dx, dy] = e.direction.map(Math.abs)
     //   if (dx === 1 && dy === 0) {
@@ -89,20 +99,44 @@ function App() {
     })
 
   return (
-    <div id="app" {...splitGuide()} style={{ width: windowWidth, height: windowHeight }} >
-      <animated.div id="split-indicator" style={{
-        transform: to(
-          [splitTranslate, splitScale],
-          ([x, y], [sx, sy]) => `translate3d(${x}px,${y}px,0) scale(${sx},${sy})`,
-        )
-      }} />
-      {panels.map(p => {
-        return (
-          <div {...bind()} key={p.id} id={p.id} className='panel' style={mapValues(p.insets, percent)} />
-        )
-      }).valueSeq()}
+    <div id="app" style={{ width: windowWidth, height: windowHeight }} >
+      <div id="main">
+        {panels.map(p => {
+          return (
+            <Spring
+              to={{
+                backgroundColor: p.selected ? style("--panel-highlight-color") : style("--panel-background-color")
+              }}>
+              {({ backgroundColor }) => <animated.div {...bind()} key={p.id} id={p.id} className='panel' style={{ backgroundColor, ...mapValues(p.insets, percent) }} />}
+            </Spring>
+          )
+        }).valueSeq()}
+      </div>
+      <div id="toolbar">
+        <button onClick={e => {
+          let p = panels.find(p => p.selected)
+          if (p != null) {
+            let [p1, p2] = splitPanel(p, "vertical")
+            setPanels(panels.remove(p.id).set(p1.id, p1).set(p2.id, p2))
+          }
+        }}>Split Vertically</button>
+        <button onClick={e => {
+          let p = panels.find(p => p.selected)
+          if (p != null) {
+            let [p1, p2] = splitPanel(p, "horizontal")
+            setPanels(panels.remove(p.id).set(p1.id, p1).set(p2.id, p2))
+          }
+        }}>Split Horizontally</button>
+      </div>
     </div >
   )
+}
+
+function style(variable: string): string {
+  return getComputedStyle(document.documentElement).getPropertyValue(variable);
+  //     --main - background - color: #0f0f0f;
+  // --panel - background - color: #1e1e1e;
+  // --panel - highlight - color: #4f4f4f;
 }
 
 // Clamps a 1D interval within a 1D bound.
@@ -115,11 +149,14 @@ function computedBounds(element: HTMLElement) {
   return [element.offsetTop, element.offsetLeft, element.offsetWidth, element.offsetHeight]
 }
 
+function gestureTarget(gesture: FullGestureState<any>) {
+  return gesture?.event?.target instanceof HTMLElement ? gesture.event.target : null
+}
 
-function useTargetElement(): [HTMLElement | null, (e: FullGestureState<any>) => void] {
+function useTargetElement(): [HTMLElement | null, (gesture: FullGestureState<any>) => void] {
   const [target, setTarget] = useState<HTMLElement | null>(null)
-  return [target, (e: FullGestureState<any> | null) => {
-    setTarget(e?.event?.target instanceof HTMLElement ? e.event.target : null)
+  return [target, (gesture: FullGestureState<any> | null) => {
+    setTarget(gesture?.event?.target instanceof HTMLElement ? gesture.event.target : null)
   }];
 }
 
@@ -136,26 +173,42 @@ function mapValues<V, U>(obj: { [s: string]: V }, fn: (v: V) => U) {
   )
 }
 
-// Creates 2 new panels from the given panel using direction to determine the axis of the split
-function splitPanel(panel: Panel, initial: Vector2, direction: Vector2, windowSize: Vector2): [Panel?, Panel?] {
+function splitPanel(panel: Panel, axis: Axis): [Panel, Panel] {
   var id1 = uuidv4()
   var id2 = uuidv4()
-  const [w, h] = windowSize
-  console.log(direction)
-  const [dx, dy] = direction
-  const [x, y] = [initial[0] / w, initial[1] / h]
-  // horizontal slice, dy=0
-  if (dx === 1 && dy === 0) {
-    var p1 = { id: id1, insets: { ...panel.insets, bottom: (1 - y) } }
-    var p2 = { id: id2, insets: { ...panel.insets, top: y } }
-    return [p1, p2]
-  } else if (dx === 0 && dy === 1) {
-    // vertical slice, dx=0
-    var p1 = { id: id1, insets: { ...panel.insets, right: (1 - x) } }
-    var p2 = { id: id2, insets: { ...panel.insets, left: x } }
-    return [p1, p2]
-  }
-  return [undefined, undefined]
+  let insets = panel.insets;
+  const { top: t, right: r, bottom: b, left: l } = insets;
+  let w = 1 - r - l
+  let h = 1 - t - b
+  let [insets1, insets2] =
+    axis === "vertical"
+      ? [{ ...insets, right: r + w / 2 }, { ...insets, left: l + w / 2 }]
+      : [{ ...insets, bottom: b + h / 2 }, { ...insets, top: t + h / 2 }]
+  return [{ id: id1, selected: true, insets: insets1 }, { id: id2, selected: true, insets: insets2 }]
 }
+
+
+// Creates 2 new panels from the given panel using direction to determine the axis of the split
+// function splitPanel(panel: Panel, initial: Vector2, direction: Vector2, windowSize: Vector2): [Panel?, Panel?] {
+//   var id1 = uuidv4()
+//   var id2 = uuidv4()
+//   const [w, h] = windowSize
+//   console.log(direction)
+//   const [dx, dy] = direction
+//   const [x, y] = [initial[0] / w, initial[1] / h]
+//   // horizontal slice, dy=0
+//   if (dx === 1 && dy === 0) {
+//     var p1 = { id: id1, insets: { ...panel.insets, bottom: (1 - y) } }
+//     var p2 = { id: id2, insets: { ...panel.insets, top: y } }
+//     return [p1, p2]
+//   } else if (dx === 0 && dy === 1) {
+//     // vertical slice, dx=0
+//     var p1 = { id: id1, insets: { ...panel.insets, right: (1 - x) } }
+//     var p2 = { id: id2, insets: { ...panel.insets, left: x } }
+//     return [p1, p2]
+//   }
+//   return [undefined, undefined]
+// }
+
 
 export default App;
--- src/junk.tsx
+++ src/junk.tsx
@@ -1,13 +1,41 @@
-function bounds(insets: Insets, containerSize: vec2): Rect {
-    let [cx, cy] = containerSize
-    return {
-        x: insets.left * cx,
-        y: insets.top * cy,
-        width: (1 - insets.right) * cx,
-        height: (1 - insets.bottom) * cy
-    }
-}
-function styleOfTransform(transform: Transform): CSSProperties {
-    let [tx, ty, sx, sy] = [...transform.translate, ...transform.scale]
-    return { transform: `translate(${tx}px,${ty}px) scale(${sx},${sy}) rotateX(0deg)` }
-}
+// export function bounds(insets: Insets, containerSize: vec2): Rect {
+//     let [cx, cy] = containerSize
+//     return {
+//         x: insets.left * cx,
+//         y: insets.top * cy,
+//         width: (1 - insets.right) * cx,
+//         height: (1 - insets.bottom) * cy
+//     }
+// }
+// export function styleOfTransform(transform: Transform): CSSProperties {
+//     let [tx, ty, sx, sy] = [...transform.translate, ...transform.scale]
+//     return { transform: `translate(${tx}px,${ty}px) scale(${sx},${sy}) rotateX(0deg)` }
+// }
+
+
+// const [{ splitTranslate }, setSplitTranslate] = useSpring(() => ({ splitTranslate: [0, 0] }))
+// const [{ splitScale }, setSplitScale] = useSpring(() => ({ splitScale: [1, 1] }))
+
+
+
+// const splitGuide = useDrag(e => {
+//     let panel = target
+//     if (panel != null) {
+//         const [x0, y0, w, h] = computedBounds(panel)
+//         let [ex, ey] = e.xy
+//         var sx = 2
+//         var sy = h
+//         var tx = clampInterval({ left: ex, width: sx }, { left: x0, width: w })
+//         var ty = x0 + h / 2
+//         setSplitTranslate({ splitTranslate: [tx, ty], immediate: true })
+//         setSplitScale({ splitScale: [sx, sy] })
+//     }
+// })
+// {...splitGuide()}
+// <animated.div id="split-indicator" style={{
+//   transform: to(
+//     [splitTranslate, splitScale],
+//     ([x, y], [sx, sy]) => `translate3d(${x}px,${y}px,0) scale(${sx},${sy})`,
+//   )
+// }} />
+export { }
